01917f492a57e782550e8252aaf889bbf3a08cd4
Eugen Godun <dinamio@ukr.net>
2018-03-24 12:00:39 +0200

    Adding sorting lab

---
+++ SortLab/src/Main.java
@@ -0,0 +1,25 @@
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.IntStream;
+
+import static java.util.stream.Collectors.toList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        List<Integer> list = IntStream.iterate(0, i -> new Random().nextInt()).limit(20).boxed().collect(toList());
+        Main.sortGodun(list);
+        list.stream().forEach(System.out::println);
+        //List<Integer> yourSurnameList = IntStream.iterate(0, i -> new Random().nextInt()).limit(20).boxed().collect(toList());
+        //Main.sortYourSurname(list);
+        //list.stream().forEach(System.out::println);
+    }
+
+    private static List<Integer> sortGodun(List<Integer> list) {
+        Collections.sort(list);
+        return list;
+    }
+
+
+}
