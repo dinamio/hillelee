5176dbc5be4864dd67cc9f81dd08184715f17f4c
AndryNosach <andry.nosach@gmail.com>
2018-03-24 16:49:31 +0200

    Insertion sort version 1

--- SortLab/src/Main.java
+++ SortLab/src/Main.java
@@ -1,4 +1,5 @@
 import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
 import java.util.stream.IntStream;
@@ -11,14 +12,38 @@ public class Main {
         List<Integer> list = IntStream.iterate(0, i -> new Random().nextInt()).limit(20).boxed().collect(toList());
         Main.sortGodun(list);
         list.stream().forEach(System.out::println);
-        //List<Integer> yourSurnameList = IntStream.iterate(0, i -> new Random().nextInt()).limit(20).boxed().collect(toList());
-        //Main.sortYourSurname(list);
-        //list.stream().forEach(System.out::println);
+//        List<Integer> nosachList = IntStream.iterate(0, i -> new Random().nextInt()).limit(20).boxed().collect(toList());
+//        Main.sortNosach(nosachList);
+//        nosachList.stream().forEach(System.out::println);
     }
 
     private static List<Integer> sortGodun(List<Integer> list) {
         Collections.sort(list);
         return list;
+    }
+
+    //сортировка вставками
+    public static List<Integer> sortNosach (List<Integer> inputList){
+        List<Integer> resultList = new LinkedList<>();
+        resultList.add(inputList.get(0));
+
+        for (int i = 1; i< inputList.size(); i++){
+
+            int sizeResult = resultList.size(); // size - в отдельной переменной т.к. во внутреннем цикле он может поменятся
+
+            for (int j=0; j<sizeResult; j++){
+                if (inputList.get(i) < resultList.get(j)){
+                    resultList.add(j, inputList.get(i));
+                    break;
+                }
+                if (j == resultList.size()-1){
+                    resultList.add(inputList.get(i));
+                }
+            }
+        }
+        inputList.clear();
+        inputList.addAll(resultList);
+        return inputList;
     }
 
 
