815116a8e630729a9570e4bd104a5ab3763f750d
romakonn1 <roman1.kononov1@gmail.com>
2018-03-24 16:51:13 +0200

    completed hw1 bubble sort

--- SortLab/src/Main.java
+++ SortLab/src/Main.java
@@ -8,18 +8,33 @@ import static java.util.stream.Collectors.toList;
 public class Main {
 
     public static void main(String[] args) {
-        List<Integer> list = IntStream.iterate(0, i -> new Random().nextInt()).limit(20).boxed().collect(toList());
-        Main.sortGodun(list);
-        list.stream().forEach(System.out::println);
-        //List<Integer> yourSurnameList = IntStream.iterate(0, i -> new Random().nextInt()).limit(20).boxed().collect(toList());
-        //Main.sortYourSurname(list);
-        //list.stream().forEach(System.out::println);
+//        List<Integer> list = IntStream.iterate(0, i -> new Random().nextInt()).limit(20).boxed().collect(toList());
+//        Main.sortGodun(list);
+//        list.stream().forEach(System.out::println);
+        List<Integer> kononovList = IntStream.iterate(0, i -> new Random().nextInt()).limit(20).boxed().collect(toList());
+        Main.sortKononov(kononovList);
+        kononovList.stream().forEach(System.out::println);
+    }
+
+    private static List<Integer> sortKononov(List<Integer> list) {
+        for (int size = list.size(); size > 0; size--) {
+            for (int i = 0; i < size - 1; i++) {
+                if (list.get(i) > list.get(i + 1)) {
+                    Main.swap(list, i, i + 1);
+                }
+            }
+        }
+        return list;
+    }
+
+    private static void swap(List<Integer> list, Integer index1, Integer index2) {
+        Integer tempI = list.get(index1);
+        list.set(index1, list.get(index2));
+        list.set(index2, tempI);
     }
 
     private static List<Integer> sortGodun(List<Integer> list) {
         Collections.sort(list);
         return list;
     }
-
-
 }
