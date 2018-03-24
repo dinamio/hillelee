8020c29ea657f27fefe14ff10199f430e032e0a6
Владислав Солопов <solovladik@mail.ru>
2018-03-24 19:29:29 +0200

    Sorting method added

--- SortLab/src/Main.java
+++ SortLab/src/Main.java
@@ -3,6 +3,7 @@ import java.util.List;
 import java.util.Random;
 import java.util.stream.IntStream;
 
+import static java.util.stream.Collectors.averagingDouble;
 import static java.util.stream.Collectors.toList;
 
 public class Main {
@@ -11,9 +12,10 @@ public class Main {
         List<Integer> list = IntStream.iterate(0, i -> new Random().nextInt()).limit(20).boxed().collect(toList());
         Main.sortGodun(list);
         list.stream().forEach(System.out::println);
-        //List<Integer> yourSurnameList = IntStream.iterate(0, i -> new Random().nextInt()).limit(20).boxed().collect(toList());
-        //Main.sortYourSurname(list);
-        //list.stream().forEach(System.out::println);
+
+        List<Integer> solopovList = IntStream.iterate(0, i -> new Random().nextInt()).limit(20).boxed().collect(toList());
+        com.company.Main.sortSolopov(solopovList);
+        solopovList.stream().forEach(System.out::println);
     }
 
     private static List<Integer> sortGodun(List<Integer> list) {
@@ -21,5 +23,24 @@ public class Main {
         return list;
     }
 
-//test: some method
+
+    private static List<Integer> sortSolopov(List<Integer> list){ //selection sort
+        int temp, j;
+        for(int i = 0; i < list.size() - 1; i++){
+            if (list.get(i) > list.get(i+1)) {
+                temp = list.get(i+1);
+                list.set(i+1, list.get(i));
+                j = i;
+                while (j > 0 && temp < list.get(j-1)) {
+                    list.set(j,list.get(j - 1));
+                    j--;
+                }
+                list.set(j,temp);
+            }
+        }
+        return list;
+    }
+
+
+
 }
