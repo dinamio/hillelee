ef145db27e5b6a10f15d1261ee91c25c8d43b099
Roman Kyrychenko <rp.kyrychenko@gmail.com>
2018-03-26 18:56:48 +0300

    add insertion sort method to Main.java

--- SortLab/src/Main.java
+++ SortLab/src/Main.java
@@ -13,6 +13,7 @@ public class Main {
     public static void main(String[] args) {
         List<Integer> list = IntStream.iterate(0, i -> new Random().nextInt()).limit(20).boxed().collect(toList());
         Main.sortGodun(list);
+        // Main.sortKyrychenko(list);
         list.stream().forEach(System.out::println);
 
 
@@ -67,6 +68,20 @@ public class Main {
         return list;
     }
 
+    // insertion sort
+    private static List<Integer> sortKyrychenko(List<Integer> list) {
 
-
+        for(int i = 1; i < list.size(); i++) {
+            for(int j = i; j > 0; j--)
+            {
+                if (list.get(j - 1) > list.get(j))
+                {
+                    Integer temp = list.get(j - 1);
+                    list.set(j - 1, list.get(j));
+                    list.set(j, temp);
+                }
+            }
+        }
+        return list;
+    }
 }
