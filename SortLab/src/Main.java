30de3658a84c68b2a0b5f20d414205d0b4e403b7
romakonn1 <roman1.kononov1@gmail.com>
2018-03-30 17:50:45 +0300

    completed hw1

--- SortLab/src/Main.java
+++ SortLab/src/Main.java
@@ -32,6 +32,28 @@ public class Main {
         sortRubinskyi(listRubnskyi);  
     }
 
+
+    //Roman***********************************************************************************************************//
+    //List<Integer> kononovList = IntStream.iterate(0, i -> new Random().nextInt()).limit(20).boxed().collect(toList());
+    //Main.sortKononov(kononovList);
+    //kononovList.stream().forEach(System.out::println);
+    private static List<Integer> sortKononov(List<Integer> list) {
+        for (int size = list.size(); size > 0; size--) {
+            for (int i = 0; i < size - 1; i++) {
+                if (list.get(i) > list.get(i + 1)) {
+                    Main.swap(list, i, i + 1);
+                }
+            }
+        }
+        return list;
+    }
+    private static void swap(List<Integer> list, Integer index1, Integer index2) {
+        Integer tempI = list.get(index1);
+        list.set(index1, list.get(index2));
+        list.set(index2, tempI);
+    }
+    //****************************************************************************************************************//
+
     private static List<Integer> sortGodun(List<Integer> list) {
         Collections.sort(list);
         return list;
@@ -40,7 +62,6 @@ public class Main {
     private static List<Integer> fillingList() {
         return IntStream.iterate(0, i -> new Random().nextInt()).limit(20).boxed().collect(toList());
     }
-
 
  private static List<Integer> sortSolopov(List<Integer> list){
         int temp, j;
