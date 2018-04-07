ddacd3babdd336d01681d6504829dea47468d5d9
bondarenkodyu <d.bondarenko.1990@gmail.com>
2018-04-08 00:45:07 +0400

    Change Main class, used OOP approach to do the sorting

--- bondarenko/sortingconsumer/src/main/java/com/bondarenko/Main.java
+++ bondarenko/sortingconsumer/src/main/java/com/bondarenko/Main.java
@@ -1,10 +1,10 @@
 package com.bondarenko;
 
-import com.bondarenko.utils.PrintUtils;
+
+import com.bondarenko.tasks.SortTask;
 import com.bondarenko.utils.SortUtils;
 
 import java.util.*;
-import java.util.function.Consumer;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -14,19 +14,13 @@ public class Main {
     public static void main(String[] args) {
         List<Integer> unsortedList = createUnsortedList(10);
 
-        doJob(unsortedList, "Quick sort", (p) -> SortUtils.quickSort(p, 0, p.size() -1));
-        doJob(unsortedList, "Bubble sort", (p) -> SortUtils.bubbleSort(p));
-        doJob(unsortedList, "Default sort", (p) -> SortUtils.defaultSort(p));
-        doJob(unsortedList, "Default sort(reverse)", (p) -> SortUtils.defaultSort(p, Comparator.reverseOrder()));
-    }
+        List<SortTask<List<Integer>>> tasks = new ArrayList<>();
+        tasks.add(new SortTask<>(unsortedList, "Quick sort", (p) -> SortUtils.quickSort(p, 0, p.size() -1)));
+        tasks.add(new SortTask<>(unsortedList, "Bubble sort", (p) -> SortUtils.bubbleSort(p)));
+        tasks.add(new SortTask<>(unsortedList, "Default sort", (p) -> SortUtils.defaultSort(p)));
+        tasks.add(new SortTask<>(unsortedList, "Default sort(reverse)", (p) -> SortUtils.defaultSort(p, Comparator.reverseOrder())));
+        tasks.forEach(SortTask::doJob);
 
-
-
-    private static void doJob(List<Integer> unsortedList, String name, Consumer<List<Integer>> consumer){
-        PrintUtils.printDelimiter(name);
-        consumer.accept(unsortedList);
-        PrintUtils.printCollection(unsortedList);
-        SortUtils.shuffle(unsortedList);
     }
 
     private static List<Integer> createUnsortedList(final int count) {
