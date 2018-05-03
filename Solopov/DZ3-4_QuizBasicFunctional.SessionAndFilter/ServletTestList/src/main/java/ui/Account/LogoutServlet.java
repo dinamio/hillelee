73625f5ffad4f891f309babe74e2f0b073c38a2b
Vladyslav <solovladik@mail.ru>
2018-05-04 01:20:27 +0300

    correct doPost() body

--- Solopov/DZ3-4_QuizBasicFunctional.SessionAndFilter/ServletTestList/src/main/java/ui/Account/LogoutServlet.java
+++ Solopov/DZ3-4_QuizBasicFunctional.SessionAndFilter/ServletTestList/src/main/java/ui/Account/LogoutServlet.java
@@ -19,6 +19,6 @@ public class LogoutServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        doGet(req,resp);
+            super.doPost(req,resp);
     }
 }
