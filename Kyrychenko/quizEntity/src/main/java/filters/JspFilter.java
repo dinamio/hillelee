f4eeb8dbe079b912848fab74e2279ae665954449
rpkyrych <rp.kyrychenko@gmail.com>
2018-05-13 15:11:21 +0300

    Added annotation to class declaration.

--- Kyrychenko/quizEntity/src/main/java/filters/JspFilter.java
+++ Kyrychenko/quizEntity/src/main/java/filters/JspFilter.java
@@ -1,11 +1,13 @@
 package filters;
 
 import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
+@WebFilter("/view/*")
 public class JspFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
