14ca0924eda1eaac024f7c4114b9492fee2d968d
Vladyslav <solovladik@mail.ru>
2018-05-23 16:01:50 +0300

    Update LoginServlet.java

--- Solopov/DZ5_AjaxAsynchronousMethodAndCSS/ServletTestList/src/main/java/ui/Account/LoginServlet.java
+++ Solopov/DZ5_AjaxAsynchronousMethodAndCSS/ServletTestList/src/main/java/ui/Account/LoginServlet.java
@@ -13,28 +13,25 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class LoginServlet extends HttpServlet {
-        private static Logger log = Logger.getLogger(LoginServlet.class.getName());
+    private static Logger log = Logger.getLogger(LoginServlet.class.getName());
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("Authorization.jsp").forward(req,resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         HttpSession session=req.getSession();
         String login,password;
         login=req.getParameter("uname");
         password=req.getParameter("psw");
         log.info(login+" "+password);
 
-        if(login!=null || password!=null){
-            if(new UserService().authorizate(login,password)){
-                 session.setAttribute("login",login);
-                 resp.sendRedirect("/quizlist");
-            }
-            else req.getRequestDispatcher("Authorization.jsp").forward(req,resp);
+        if((login!=null || password!=null)&&(new UserService().authorizate(login,password))){
+            session.setAttribute("login",login);
+            resp.sendRedirect("/quizlist");
         }
         else req.getRequestDispatcher("Authorization.jsp").forward(req,resp);
-    }
-
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        doGet(req,resp);
     }
 }
