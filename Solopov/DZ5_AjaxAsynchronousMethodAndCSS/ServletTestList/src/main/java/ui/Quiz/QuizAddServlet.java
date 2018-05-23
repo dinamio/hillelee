6b54d815ef8d5a0ee5602be61538ba1f23746b86
Vladyslav <solovladik@mail.ru>
2018-05-23 16:30:21 +0300

    Update QuizAddServlet.java

--- Solopov/DZ5_AjaxAsynchronousMethodAndCSS/ServletTestList/src/main/java/ui/Quiz/QuizAddServlet.java
+++ Solopov/DZ5_AjaxAsynchronousMethodAndCSS/ServletTestList/src/main/java/ui/Quiz/QuizAddServlet.java
@@ -2,7 +2,6 @@ package ui.Quiz;
 
 import entity.Quiz;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -12,17 +11,16 @@ import java.io.IOException;
 public class QuizAddServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        RequestDispatcher dispatcher = req.getRequestDispatcher("/quizlist");
-        boolean formAddTest= Boolean.parseBoolean(req.getParameter("formCreateQuiz"));
+        boolean formAddTest = Boolean.parseBoolean(req.getParameter("formCreateQuiz"));
 
-        if(formAddTest){
+        if (formAddTest) {
             String testTitle = req.getParameter("testTitle");
             String objective = req.getParameter("objective");
-            Quiz quiz =new Quiz(testTitle,objective);
+            Quiz quiz = new Quiz(testTitle, objective);
             quiz.setCreator(req.getSession().getAttribute("login").toString());
             QuizListServlet.quizList.add(quiz);
         }//end formAddTest
 
-        dispatcher.forward(req, resp);
+        resp.sendRedirect("/quizlist");
     }
 }
