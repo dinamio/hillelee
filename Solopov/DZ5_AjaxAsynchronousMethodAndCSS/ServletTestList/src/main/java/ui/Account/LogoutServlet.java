74ec8a152030d2859766e861cc224a2ddcf152a0
Vladyslav <solovladik@mail.ru>
2018-05-23 16:03:57 +0300

    Update LogoutServlet.java

--- Solopov/DZ5_AjaxAsynchronousMethodAndCSS/ServletTestList/src/main/java/ui/Account/LogoutServlet.java
+++ Solopov/DZ5_AjaxAsynchronousMethodAndCSS/ServletTestList/src/main/java/ui/Account/LogoutServlet.java
@@ -17,9 +17,4 @@ public class LogoutServlet extends HttpServlet {
 
 
     }
-
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req,resp);
-    }
 }
