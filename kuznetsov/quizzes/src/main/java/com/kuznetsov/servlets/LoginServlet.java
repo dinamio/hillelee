0d1044a0975ea3cd74e49662ab95bd0d7068e024
muao <144kuznetsov@gmail.com>
2018-05-26 17:22:42 +0300

    homeWork#8 shift login servlet to controller

--- kuznetsov/quizzes/src/main/java/com/kuznetsov/servlets/LoginServlet.java
+++ kuznetsov/quizzes/src/main/java/com/kuznetsov/servlets/LoginServlet.java
@@ -4,43 +4,60 @@ import dao.impl.QuizDaoImpl;
 
 import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.context.support.SpringBeanAutowiringSupport;
 
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletConfig;
-import javax.servlet.ServletException;
+import javax.servlet.*;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-@WebServlet("")
+import static org.springframework.web.bind.annotation.RequestMethod.*;
+
+@Controller
+//@WebServlet("")
 public class LoginServlet extends HttpServlet {
 
     @Autowired
     private QuizDaoImpl quizDao;
 
-    @Override
+    /*@Override
     public void init(ServletConfig config) throws ServletException {
         super.init(config);
         SpringBeanAutowiringSupport.processInjectionBasedOnServletContext(this,
                 config.getServletContext());
-    }
+    }*/
 
 
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    /*@Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {*/
+
+    @RequestMapping(method = GET, value = "")
+            public void getLogin(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+
+        HttpServletRequest req = (HttpServletRequest) servletRequest;
 
         RequestDispatcher loginDispatcher = req.getRequestDispatcher("/view/login.jsp");
         if (req.getSession().getAttribute("wrongMessage") == null) {
             req.getSession().setAttribute("wrongMessage", "");
         }
-        loginDispatcher.include(req, resp);
+        loginDispatcher.include(servletRequest, servletResponse);
     }
 
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    /*@Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {*/
+
+    @RequestMapping(method = POST,value = "")
+            public void postLogin(ServletRequest servletRequest, ServletResponse servletResponse) throws IOException, ServletException {
+
+        HttpServletRequest req = (HttpServletRequest) servletRequest;
+        HttpServletResponse resp = (HttpServletResponse) servletResponse;
+
 
         String buttonType = req.getParameter("submit");
         String login = req.getParameter("login");
