23c43f0d71fb861263541b7f0f2702f33744dfb7
rpkyrych <rp.kyrychenko@gmail.com>
2018-06-01 17:04:58 +0300

    delete unused files

---
+++ Kyrychenko/getsort/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,8 @@
+
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
+:
+
+.gitignore,a\5\a5cc2925ca8258af241be7e5b0381edf30266302
+R
+"src\main\java\getsort\GetSort.java,1\6\165863e746ab93d75634e42915889307c056966a
---
+++ Kyrychenko/getsort/src/test/java/getsort/GetSortTest.java
@@ -0,0 +1,20 @@
+package getsort;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple GetSort.
+ */
+public class GetSortTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
